44eb03a678212a0090e76ca22cf65ee8986fbd4c
sarita-coder <sarita.roman.colonio@gmail.com>
2016-12-10 08:35:29 -0500

    AGRGANDO LANDINGS

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// $(document).ready(function() {
+// 	$('#menu-des').on('click',function(){
+// 	  $('#main-nav').toggleClass('mostrar')
+// 	  $('#menu-des').toggleClass('mover')
+// 	})
+// 	$('#setting').on('click', function(){
+// 		$('#lenguages').toggleClass('mostrar')
+// 	})
+// }
+
